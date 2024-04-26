e6703632a2682b9d53c3adde3bd0c01d9bf4eea1
meabit1 <bahrimohamedelamine22esi@gmail.com>
2024-04-26 22:43:24 +0100

    added link to source code

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ export default function App() {
 
   return (
     <div>
-      <div className="text-center bg-blue-500 text-white font-bold text-7xl">
-        HILL CIPHER <div className="text-base">Bahri Mohamed El Amine</div>
+      <div className="text-center bg-blue-500 text-white font-bold p-2 text-7xl">
+        HILL CIPHER <div className="text-base">Bahri Mohamed El Amine <a className="bg-green-500 rounded-md p-1" target="_blank" href="https://github.com/meabit1/hill-cipher">Source Code</a></div>
       </div>
       <div className="flex flex-row justify-center items-center h-screen">
         <div>
